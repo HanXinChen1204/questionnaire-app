eb19d4fa5c73837927dfc3747a106729d65bf183
Addis <1791872995@qq.com>
2021-06-23 14:35:34 +0800

    继续汉化并修改答题逻辑

--- app/src/main/java/com/example/android/questionnaire/data/QuestionSet.java
+++ app/src/main/java/com/example/android/questionnaire/data/QuestionSet.java
@@ -19,7 +19,7 @@ public class QuestionSet {
         Question question1 = new Question(context.getString(R.string.q1), RADIOBUTTON, context.getResources().getStringArray(R.array.q1_options), Collections.singletonList(0));
         questions.add(question1);
 
-        Question question2 = new Question(context.getString(R.string.q2), Options.CHECKBOX, context.getResources().getStringArray(R.array.q2_options), Arrays.asList(0, 1));
+        Question question2 = new Question(context.getString(R.string.q2), Options.CHECKBOX, context.getResources().getStringArray(R.array.q2_options), Arrays.asList(0, 1, 2));
         questions.add(question2);
 
         Question question3 = new Question(context.getString(R.string.q3), Options.EDITTEXT, context.getString(R.string.q3_answer));
@@ -28,10 +28,10 @@ public class QuestionSet {
         Question question4 = new Question(context.getString(R.string.q4), RADIOBUTTON, context.getResources().getStringArray(R.array.q4_options), Collections.singletonList(3));
         questions.add(question4);
 
-        Question question5 = new Question(context.getString(R.string.q5), CHECKBOX, context.getResources().getStringArray(R.array.q5_options), Arrays.asList(0, 1, 3));
+        Question question5 = new Question(context.getString(R.string.q5), CHECKBOX, context.getResources().getStringArray(R.array.q5_options), Arrays.asList(0, 2, 3));
         questions.add(question5);
 
-        Question question6 = new Question(context.getString(R.string.q6), RADIOBUTTON, context.getResources().getStringArray(R.array.q6_options), Collections.singletonList(1));
+        Question question6 = new Question(context.getString(R.string.q6), RADIOBUTTON, context.getResources().getStringArray(R.array.q6_options), Collections.singletonList(2));
         questions.add(question6);
 
         Question question7 = new Question(context.getString(R.string.q7), Options.EDITTEXT, context.getString(R.string.q7_answer));
